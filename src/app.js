85a6bbda670a5807fb3f77142bec4f59a111d7dc
Kelvyn Santana <kelvyn.santana@icloud.com>
2019-08-19 23:41:36 -0300

    Criando Model de Produto

--- src/app.js
+++ src/app.js
@@ -4,11 +4,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const router = express.Router();
+const mongoose = require('mongoose');
 //FINAL IMPORTS
 
 //LOAD ROUTES
 const indexRoute = require('./routes/indexRoutes');
 const productRoute = require('./routes/productRoutes');
+
+//CONECTA O BANCO DE DADOS
+mongoose.connect('mongodb+srv://doublepeppers:kss046dp@doublepeppers-tswtt.azure.mongodb.net/nodestr?retryWrites=true&w=majority')
 
 //BODY PARSER
 app.use(bodyParser.json());
